3562f131a05c2ef06c584f3971ac0ebffa56ee60
taz-jt <tazdik.ershad@arbetsformedlingen.se>
2019-09-06 10:51:20 +0200

    demo login page done

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const config = require('./lib/config');
 const ejs = require('ejs');
 const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 
 app.set('views', __dirname+'/views');
 app.set('view engine', 'ejs');
 app.engine('html', ejs.__express);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
 app.use('/css', express.static(__dirname+'/public/css'));
 app.use('/img', express.static(__dirname+'/public/img'));
 app.use('/js', express.static(__dirname+'/public/js'));
@@ -17,5 +21,30 @@ app.get('/', (req, res) => {
     let templateData = {username: "Maximillian"};
     res.render('index', templateData);
 });
+
+app.get('/login', (req, res) => {
+    let templateData = {
+        title: 'Login'
+    }
+    res.render('login', templateData)
+});
+
+app.post('/login', (req, res) => {
+    const userAuthInfo = {
+        username: req.body.username,
+        password: req.body.password
+    }
+
+    console.log(userAuthInfo);
+
+    /*
+    axios.post('auth1arbetsformedlingen.se/Password/PasswordSERULET', userAuthInfo)
+        .then(res => console.log(res))
+        .catch(error => console.log(error))
+    */
+
+    res.send('done')
+
+})
 
 app.listen(config.port, () => console.log(`Gravity Demo Site listening on port ${config.port}!`))
