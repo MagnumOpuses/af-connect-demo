29b2d4f69a9ae0b68ab1167ac6f6687085fb6c8e
Anton Wiklund <anton.wiklund@arbetsformedlingen.se>
2019-09-10 12:44:53 +0200

    Use case and beacon integration (#2)
    * Implementation of the demo use-case and adopted Gravity Beacon.
    
    * WIP: Upgraded bootstrap to v4, crude viewer of CV document and improved styling of demo pages.
    
    * Renamed '/application' endpoint to '/inspect'. Styled the content of cv 'employment' and 'qualifications'.

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const config = require('./lib/config');
 const ejs = require('ejs');
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+const jwtService = require('./lib/jwt-service');
+const portabilityApi = require('./lib/portability-api');
+
+function getRequestCookie(req, name) {
+    var value = '; ' + req.headers.cookie;
+    var parts = value.split('; ' + name + '=');
+    if (parts.length == 2) return parts.pop().split(';').shift();
+}
 
 app.set('views', __dirname+'/views');
 app.set('view engine', 'ejs');
 app.engine('html', ejs.__express);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use('/css', express.static(__dirname+'/public/css'));
 app.use('/img', express.static(__dirname+'/public/img'));
 app.use('/js', express.static(__dirname+'/public/js'));
@@ -13,9 +25,30 @@ app.use('/fonts', express.static(__dirname+'/public/fonts'));
 app.use('/vendor', express.static(__dirname+'/public/vendor'));
 app.use('/favicon.ico', express.static(__dirname+'/public/favicon.ico'));
 
-app.get('/', (req, res) => {
-    let templateData = {username: "Maximillian"};
-    res.render('index', templateData);
+app.get('/', (req, res) => { res.render('pages/index'); });
+app.get('/import', (req, res) => { res.render('pages/import'); });
+app.get('/inspect', (req, res) => { res.render('pages/inspect'); });
+app.get('/complete', (req, res) => { res.render('pages/complete'); });
+
+app.post('/form', (req, res) => {
+    res.render('partials/form', { data: req.body });
+});
+
+app.get('/cv', (req, res) => {
+    let cookie = getRequestCookie(req, config.ssoCookieName);
+    if (cookie === undefined) {
+        console.log('No cookie supplied');
+        res.sendStatus(401);
+        return;
+    }
+
+    jwtService.token(cookie)
+    .then(token => portabilityApi.cv(token))
+    .then(cv => res.send(cv))
+    .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+    });
 });
 
 app.listen(config.port, () => console.log(`Gravity Demo Site listening on port ${config.port}!`))
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,70 @@
 "use strict"
 
-console.log('Welcome to Gravity Demo Site');
+function createAccordionCard(id, label, body) {
+    let card = $('<div class="card"></div>');
+    let cardId = 'card_' + id;
+    let cardHeaderId = cardId + '_header';
+    let cardContentId = cardId + '_content';
+    
+    let cardHeader = $('<div class="card-header"></div>')
+        .attr('id', cardHeaderId);
+    card.append(cardHeader);
+
+    let cardButton = $('<button class="btn btn-link" type="button" data-toggle="collapse"></button>')
+        .attr('data-target', '#' + cardContentId)
+        .html(label);
+    cardHeader.append(cardButton);
+
+    let cardContent = $('<div class="collapse" data-parent="#cvAccordion"></div>')
+        .attr('id', cardContentId)
+        .append(body);
+    card.append(cardContent);
+
+    return card;
+}
+
+function onAuth() {
+    $('#feedback').html('För att hämta ditt CV från Arbetsförmedlingen måste du logga in.');
+    $('#feedback').show();
+}
+
+function onFetch() {
+    $('#feedback').html('Hämtar CV från Arbetsförmedlingen...');
+    $('#feedback').show();
+}
+
+function onResponse(data) {
+
+    let cv = data;
+
+    fetch('/form', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(cv)
+    }).then(response => {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+            return;
+        }
+
+        return response.text();
+    })
+    .then(form => {
+        $('#cvAccordion').append(form);
+    })
+    .catch(err => console.log('Fetch Error :-S', err));
+
+    let person = cv['person'];
+    let communication = cv['communication'];
+
+    let accordion = $('#cvAccordion').css('text-align', 'left');
+
+    console.log(cv);
+
+    $('#feedback').hide();
+    $('#resultPane').show();
+    $('#consentControl').show();
+}
