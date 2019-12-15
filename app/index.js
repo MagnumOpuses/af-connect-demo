5e6c2cc728c542ee1461b50e0c383f04bff613eb
Anton Wiklund <anton.wiklund@arbetsformedlingen.se>
2019-12-15 10:51:37 +0100

    Stage (#14)
    * Initial scaffolding for site.
    
    * Slight adjustment to Readme content
    
    * Use case and beacon integration (#2)
    
    * Implementation of the demo use-case and adopted Gravity Beacon.
    
    * WIP: Upgraded bootstrap to v4, crude viewer of CV document and improved styling of demo pages.
    
    * Renamed '/application' endpoint to '/inspect'. Styled the content of cv 'employment' and 'qualifications'.
    
    * job application demo form populate with CV data
    
    * Added a cv data input form and som refactoring
    
    * Fixed UX, added save CV button
    
    * Fixed UX around phone field
    
    * updated readme for demosite installation
    
    * Adding dockerfile for easier deployment (#3)
    
    * Added extra EJS property checks.
    
    * Created Dockerfile that starts Nginx and Nodejs. Supervisord handles the processes.
    
    * Updated readme, and removed a comment from Dockerfile
    
    * Updated readme
    
    * Fixed deploy (#6)
    
    * Fixed deploy
    
    * Added possibilities to start without SSL configuration.
    And created a .env-example file for convenience.
    
    * removed hostname in server config
    
    * Update README.md
    
    * Separation of concerns (#7)
    
    * Fixed deploy
    
    * Removed all functionality that should not reside in this service/project.
    
    * Update package.json
    
    * Updated .env-example
    
    * Feature/better form (#8)
    
    * Improvements to the cv form
    
    * End-to-end success and design
    
    * Fix/portability url (#9)
    
    * Improvements to the cv form
    
    * End-to-end success and design
    
    * Portability url from environment vars
    
    * Fix logotype. (#10)
    
    * Using envelope (#12)
    
    * Using envelope and api-key for portability
    
    * Moved sources to app folder (#13)
    
    * Stage (#11)
    
    * Initial scaffolding for site.
    
    * Slight adjustment to Readme content
    
    * Use case and beacon integration (#2)
    
    * Implementation of the demo use-case and adopted Gravity Beacon.
    
    * WIP: Upgraded bootstrap to v4, crude viewer of CV document and improved styling of demo pages.
    
    * Renamed '/application' endpoint to '/inspect'. Styled the content of cv 'employment' and 'qualifications'.
    
    * job application demo form populate with CV data
    
    * Added a cv data input form and som refactoring
    
    * Fixed UX, added save CV button
    
    * Fixed UX around phone field
    
    * updated readme for demosite installation
    
    * Adding dockerfile for easier deployment (#3)
    
    * Added extra EJS property checks.
    
    * Created Dockerfile that starts Nginx and Nodejs. Supervisord handles the processes.
    
    * Updated readme, and removed a comment from Dockerfile
    
    * Updated readme
    
    * Fixed deploy (#6)
    
    * Fixed deploy
    
    * Added possibilities to start without SSL configuration.
    And created a .env-example file for convenience.
    
    * removed hostname in server config
    
    * Update README.md
    
    * Separation of concerns (#7)
    
    * Fixed deploy
    
    * Removed all functionality that should not reside in this service/project.
    
    * Update package.json
    
    * Updated .env-example
    
    * Feature/better form (#8)
    
    * Improvements to the cv form
    
    * End-to-end success and design
    
    * Fix/portability url (#9)
    
    * Improvements to the cv form
    
    * End-to-end success and design
    
    * Portability url from environment vars
    
    * Fix logotype. (#10)
    
    * Using envelope and api-key for portability
    
    * Moved sources to app folder

---
+++ app/index.js
@@ -0,0 +1,50 @@
+const config = require("./lib/config");
+const ejs = require("ejs");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const http = require("http");
+
+let server = http.createServer(app);
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", ejs.__express);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/css", express.static(__dirname + "/public/css"));
+app.use("/img", express.static(__dirname + "/public/img"));
+app.use("/js", express.static(__dirname + "/public/js"));
+app.use("/fonts", express.static(__dirname + "/public/fonts"));
+app.use("/vendor", express.static(__dirname + "/public/vendor"));
+app.use("/favicon.ico", express.static(__dirname + "/public/favicon.ico"));
+
+// Helper function to check existance and get nested properties
+let get = function() {
+  var args = Array.prototype.slice.call(arguments, 0);
+  var path = args[0].split(".");
+  var root = this;
+  for (var i = 0; i < path.length; i++) {
+    if (root[path[i]] === void 0) {
+      return args[1] ? args[1] : null;
+    } else {
+      root = root[path[i]];
+    }
+  }
+  return root;
+};
+
+app.get("/", (req, res) => {
+  res.locals.get = get;
+  res.render("pages/index", { portabilityUrl: config.portabilityUrl });
+});
+
+app.post("/cvForm", (req, res) => {
+  res.locals.get = get;
+  res.render("partials/cv-form", { data: req.body });
+});
+
+server.listen(config.port, config.host, () =>
+  console.log(`AF Connect Demo listening on: ${config.host}:${config.port} !`)
+);
---
+++ app/public/js/main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+function onResponse(envelope) {
+  console.log(envelope);
+
+  // Get the data(CV) from the envelope
+  let cv = envelope.data.data;
+
+  fetch("/cvForm", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(cv)
+  })
+    .then(response => {
+      if (response.status !== 200) {
+        console.log(
+          "Looks like there was a problem. Status code: " + response.status
+        );
+        return;
+      }
+
+      return response.text();
+    })
+    .then(form => {
+      $("#cv-data-holder")
+        .empty()
+        .append(form);
+
+      $("#feedback-text").html("Vi har hämtat ditt CV från Arbetsförmedlingen");
+      $("#create-cv-button").val("Förhandsgranska");
+      $("#af-connect-button input").val("Skicka ansökan");
+    })
+    .catch(err => console.log("Fetch error:", err));
+}
